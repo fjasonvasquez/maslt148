b2615f9a5685741e93943c10dbf15e5375920018
fjasonvasquez <fjvasquez@aol.com>
2017-03-01 23:55:29 -0800

    Integrated new and create actions for website

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -2,4 +2,21 @@ class WebsitesController < ApplicationController
 	def index
 		@website_items = Website.all
 	end
+
+	def new
+		@website_item = Website.new
+	end
+
+	def create
+    @website_item = Website.new(params.require(:website_item).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @website_item.save
+        format.html { redirect_to websites_path, notice: 'Your website item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
