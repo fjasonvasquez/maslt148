8c0e024b6341f4da5401cae98bd088dba90694ac
fjasonvasquez <fjvasquez@aol.com>
2017-03-07 23:55:32 -0800

    Integrated database relationship between topics and blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306075849) do
+ActiveRecord::Schema.define(version: 20170308074936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 20170306075849) do
     t.datetime "updated_at",             null: false
     t.string   "slug"
     t.integer  "status",     default: 0
+    t.integer  "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
+    t.index ["topic_id"], name: "index_blogs_on_topic_id", using: :btree
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -44,6 +46,12 @@ ActiveRecord::Schema.define(version: 20170306075849) do
     t.datetime "updated_at",       null: false
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "websites", force: :cascade do |t|
     t.string   "title"
     t.string   "subtitle"
@@ -54,4 +62,5 @@ ActiveRecord::Schema.define(version: 20170306075849) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "blogs", "topics"
 end
