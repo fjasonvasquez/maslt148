4e93f9ae6579695cf1d5c6be4ba551228c0bfe8b
fjasonvasquez <fjvasquez@aol.com>
2017-03-24 00:00:49 -0700

    integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.2'
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
+
+gem 'ma_view_tool', git: 'https://github.com/fjasonvasquez/ma_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
   		content_tag(:p, greeting, class: "source-greeting")
   	end
   end
+
+  def copyright_generator
+    MaViewTool::Renderer.copyright 'MA', 'All rights reserved.'
+  end
 end
