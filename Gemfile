061197ae1812f4ed8994ed2c6c55f53da80be7ff
fjasonvasquez <fjvasquez@aol.com>
2017-04-15 01:29:34 -0700

    Implemented initial connection to twitter API

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'https://rubygems.org'
+source 'http://rubygems.org'
 
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
@@ -45,3 +45,4 @@ gem 'carrierwave-aws', '~> 1.1'
 gem 'dotenv-rails', '~> 2.2'
 gem 'cocoon', '~> 1.2', '>= 1.2.9'
 gem 'gritter', '~> 1.2'
+gem 'twitter', '~> 6.1'
---
+++ lib/social_tool.rb
@@ -0,0 +1,19 @@
+module SocialTool
+	def self.twitter_search
+		client = Twitter::Streaming::Client.new do |config|
+		  config.consumer_key        = ENV.fetch("TWITTER_CONSUMER_KEY")
+		  config.consumer_secret     = ENV.fetch("TWITTER_CONSUMER_SECRET")
+		  config.access_token        = ENV.fetch("TWITTER_ACCESS_TOKEN")
+		  config.access_token_secret = ENV.fetch("TWITTER_ACCESS_SECRET")
+		end
+
+		client.search("#food", result_type: 'recent').take(6).collect do |tweet|
+			"#{tweet.user.screen_name}: #{tweet.text}"
+		end
+	end
+end
+
+TWITTER_CONSUMER_KEY=
+TWITTER_CONSUMER_SECRET=
+TWITTER_ACCESS_TOKEN=
+TWITTER_ACCESS_SECRET=
