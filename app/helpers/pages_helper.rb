885aec5133d3bbc747c140edb45fa429ffbc946c
fjasonvasquez <fjvasquez@aol.com>
2017-05-08 01:55:49 -0700

    Updated twitter feed

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -19,7 +19,7 @@ module PagesHelper
     }ix
 
     tweet.gsub(regex) do |url|
-    	"<a href=#{url} target='_blank'>#{url}</a>"
+    	"<a href='#{url}' target='_blank'>#{url}</a>"
     end.html_safe
 	end
 end
